7d8c9deeb19a02983d563dc1ce9487e710e53e07
Tamara Korol <tamarakorol2008@gmail.com>
2026-05-25 20:01:37 +0300

    Fix GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "./",
+  base: "/portfolio3/",
   plugins: [vue()],
   build: {
     outDir: "dist/pr9",
